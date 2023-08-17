f65cc016a6bcd8d539afaf579870807b07560d16
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-17 16:03:11 +0300

    Complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
+
 #!/usr/bin/env python3
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()  
+
 
 def add(num1, num2):
     pass
+    return num1 + num2;
 
 def halve(number):
     pass
+    return number/2
